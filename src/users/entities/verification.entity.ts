17fb93572cf55d61eff6e28be58bdd32d56e201d
lsw1164 <qkqkfo@gmail.com>
2021-01-30 22:53:15 +0900

    Create vefirication entity

---
+++ src/users/entities/verification.entity.ts
@@ -0,0 +1,23 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Column, Entity, JoinColumn, OneToOne, BeforeInsert } from 'typeorm';
+import { User } from './user.entity';
+import { v4 as uuidv4 } from 'uuid';
+
+@InputType({ isAbstract: true })
+@ObjectType()
+@Entity()
+export class Verification extends CoreEntity {
+  @Column()
+  @Field((type) => String)
+  code: string;
+
+  @OneToOne((type) => User)
+  @JoinColumn()
+  user: User;
+
+  @BeforeInsert()
+  createCode(): void {
+    this.code = uuidv4();
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,11 +9,14 @@ import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 import { JwtService } from 'src/jwt/jwt.service';
 import { EditProfileInput } from './dtos/edit-profile.dto';
+import { Verification } from './entities/verification.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
+    @InjectRepository(Verification)
+    private readonly verifications: Repository<Verification>,
     private readonly jwtService: JwtService,
   ) {}
   async createAccount({
@@ -26,7 +29,10 @@ export class UsersService {
       if (exists) {
         return { ok: false, error: 'There is a user with that email already' };
       }
-      await this.users.save(this.users.create({ email, password, role }));
+      const user = await this.users.save(
+        this.users.create({ email, password, role }),
+      );
+      await this.verifications.save(this.verifications.create({ user }));
       return { ok: true };
     } catch {
       return {
@@ -66,6 +72,8 @@ export class UsersService {
     const user = await this.users.findOne(userId);
     if (email) {
       user.email = email;
+      user.verified = false;
+      await this.verifications.save(this.verifications.create({ user }));
     }
     if (password) {
       user.password = password;
