baac13ce680b61f8e0d436c0f4f5fc8696de7834
lsw1164 <qkqkfo@gmail.com>
2021-01-30 15:52:40 +0900

    Add editProfile Mutation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,6 +8,7 @@ import {
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 import { JwtService } from 'src/jwt/jwt.service';
+import { EditProfileInput } from './dtos/edit-profile.dto';
 
 @Injectable()
 export class UsersService {
@@ -56,5 +57,19 @@ export class UsersService {
 
   async findById(id: number): Promise<User> {
     return this.users.findOne({ id });
+  }
+
+  async editProfile(
+    userId: number,
+    { email, password }: EditProfileInput,
+  ): Promise<User> {
+    const user = await this.users.findOne(userId);
+    if (email) {
+      user.email = email;
+    }
+    if (password) {
+      user.password = password;
+    }
+    return this.users.save(user);
   }
 }
