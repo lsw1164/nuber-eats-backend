662cc59ac8bb8aa8bfa84eb5c58145be58a3a819
lsw1164 <qkqkfo@gmail.com>
2021-02-09 01:13:47 +0900

    Test : MailService.sendVerificationEmail

---
+++ src/mail/mail.service.spec.ts
@@ -0,0 +1,55 @@
+import { Test } from '@nestjs/testing';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailModuleOptions } from './mail.interfaces';
+import { MailService } from './mail.service';
+
+const testOptions: MailModuleOptions = {
+  apiKey: 'test-apiKey',
+  domain: 'test-domain',
+  fromEmail: 'test-fromEmail',
+};
+
+describe('MailService', () => {
+  let service: MailService;
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        MailService,
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: testOptions,
+        },
+      ],
+    }).compile();
+    service = module.get<MailService>(MailService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('sendVerificationEmail', () => {
+    it('should call sendEmail', () => {
+      const sendVerificationEmailArgs = {
+        email: 'email',
+        code: 'code',
+      };
+      jest.spyOn(service, 'sendEmail').mockImplementation(async () => {});
+      service.sendVerificationEmail(
+        sendVerificationEmailArgs.email,
+        sendVerificationEmailArgs.code,
+      );
+      expect(service.sendEmail).toHaveBeenCalledTimes(1);
+      expect(service.sendEmail).toHaveBeenCalledWith(
+        'Verify Your Email',
+        'verify-email',
+        [
+          { key: 'code', value: sendVerificationEmailArgs.code },
+          { key: 'username', value: sendVerificationEmailArgs.email },
+        ],
+      );
+    });
+  });
+
+  it.todo('sendEmail');
+});
