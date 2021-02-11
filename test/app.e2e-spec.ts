ca227fe3706fe24c5c6af655cc55ef0b15d4eca6
lsw1164 <qkqkfo@gmail.com>
2021-02-12 02:26:03 +0900

    Test e2e : createAccount

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from './../src/app.module';
 import { getConnection } from 'typeorm';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -20,7 +29,47 @@ describe('AppController (e2e)', () => {
     app.close();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    const EMAIL = 'test@gmail.com';
+    const QUERY = `
+              mutation {
+                createAccount(input: {
+                  email:"${EMAIL}", 
+                  password : "12345",
+                  role: Owner
+                }) {
+                  ok,
+                  error
+                }
+              }
+        `;
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({ query: QUERY })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount).toEqual({
+            ok: true,
+            error: null,
+          });
+        });
+    });
+
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({ query: QUERY })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount).toEqual({
+            ok: false,
+            error: 'There is a user with that email already',
+          });
+        });
+    });
+  });
+
   it.todo('userProfile');
   it.todo('login');
   it.todo('me');
