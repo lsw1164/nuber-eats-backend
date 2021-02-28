6434906a1e6f86529d289714e738d70a6cd52305
swlee <swlee@bitmango.com>
2021-03-01 03:13:26 +0900

    Implement : createOrder

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { Role } from 'src/auth/role.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { CreateOrderIntput, CreateOrderOutput } from './dtos/create-order-dto';
+import { Order } from './entities/order.entity';
+import { OrdersService } from './orders.service';
 
-@Resolver()
-export class OrdersResolver {}
+@Resolver((of) => Order)
+export class OrdersResolver {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Mutation((returns) => CreateOrderOutput)
+  @Role(['Client'])
+  async createOrder(
+    @AuthUser() customer: User,
+    @Args('input') createOrderIntput: CreateOrderIntput,
+  ): Promise<CreateOrderOutput> {
+    return this.ordersService.createOrder(customer, createOrderIntput);
+  }
+}
