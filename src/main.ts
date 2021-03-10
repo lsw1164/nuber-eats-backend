efbca252e3a21462a7062ca80e61d0241e64bf09
lsw1164 <qkqkfo@gmail.com>
2021-03-10 17:19:55 +0900

    Change PortNumber

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
